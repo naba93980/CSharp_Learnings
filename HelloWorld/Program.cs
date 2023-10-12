2968e0e52201e411a591dd6f195255dc0163dbfc
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-12 18:38:16 +0530

    basic program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program1
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
