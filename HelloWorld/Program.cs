9b7e06e2cc8475b789170878f1288e13675cb15c
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-12 19:28:19 +0530

    checked, unchecked keywords

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,26 @@ namespace HelloWorld
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            checked
+            {
+                byte n = 255;
+                byte m = 255;
+
+                // overflow checking happens here
+                try
+                {
+                    m = (byte)(m + 1);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                // unchecked keyword makes sure not to do overflow checking
+                n = unchecked((byte)(n + 1));
+                Console.WriteLine(n);
+            }
         }
     }
 }
