850edf5563bdf1649f5ce52e7aeca86a04cbae7f
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-13 16:32:34 +0530

    string formatting

---
+++ StringApp/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "John";
+        int age = 30;
+
+        // composite formatting
+        Console.WriteLine("{0},{1}",name,age);
+
+        // String Interpolation
+        string message = $"My name is {name} and I am {age} years old.";
+        Console.WriteLine(message);
+    }
+}
