c9ebf1b6c5c57b3f2f5e1742eb7c926bd0d8b3bf
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-13 16:32:04 +0530

    variables, types, conversions

---
+++ VariablesTypesOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Variables;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Variable v1 = new Variable();
+        v1.run();
+    }
+}
+
---
+++ VariablesTypesOperators/Variable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Variable
+    {
+        public void run()
+        {
+            byte number = 2;
+            Console.WriteLine(number);
+
+            float floatNumber = 20.94f;
+            Console.WriteLine(floatNumber);
+
+            Char character = 'A';
+            Console.WriteLine(character);
+
+            string line = "Nabajyoti";
+            Console.WriteLine(line);
+
+            var n = 5;
+            Console.WriteLine(n);
+
+            String ax = "2";
+
+            // parsing
+            int x = Convert.ToInt32(ax);
+            int y = int.Parse(ax);
+            Console.WriteLine("{0}, {1}", x,y);
+
+            bool v  = Convert.ToBoolean("true");
+            Console.WriteLine(v);
+
+            bool v2  = Convert.ToBoolean("gfg"); // wont work
+            Console.WriteLine(v2);
+
+        }
+    }
+}
