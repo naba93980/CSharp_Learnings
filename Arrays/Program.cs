9fde7f52d4d7ab496e80e8e216a08881d7eaffe5
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-15 14:57:03 +0530

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = new string[] { "naba", "modak" };
+            Console.WriteLine(s[0]);
+            Console.WriteLine(s[1]);
+        }
+    }
+}
