aee6859c442737750806abd5b5b04a2957bae158
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-16 03:21:48 +0530

    Enum type

---
+++ Non-Primitive-Types/Enums/Databases.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+namespace Non_Primitive_Types.Enums
+{
+    public enum Databases
+    {
+        ORACLE = 5, MYSQL = 10, SQLSERVER = 15
+    }
+}
+
+
--- Non-Primitive-Types/Program.cs
+++ Non-Primitive-Types/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Non_Primitive_Types.Class;
 using Non_Primitive_Types.Class.Math;
+using Non_Primitive_Types.Enums;
 using Non_Primitive_Types.Struct;
 
 
@@ -9,6 +10,8 @@ namespace Non_Primitive_Types
     {
         static void Main(string[] args)
         {
+
+            // Class
             Person person = new Person();
             person.firstName = "Naba";
             person.lastName = "modak";
@@ -17,9 +20,23 @@ namespace Non_Primitive_Types
             Calculator cal = new Calculator();
             Console.WriteLine(cal.add(5, 7));
 
+            // Struct
             Person2 person2 = new Person2(1, "Naba");
             Console.WriteLine(person2.Id);
             Console.WriteLine(person2.Name);
+
+            // Enum
+            Databases server = Databases.SQLSERVER;                  // every enum constant here is an instance of the Database enum class which inherits Enum class
+            Console.WriteLine(server.ToString());                                // calls server.ToString() automatically
+            Console.WriteLine(server.GetHashCode());
+            Console.WriteLine((int)server);
+
+            Console.WriteLine(server.GetType());                                // Databases -> Enums -> ValueType -> Object
+
+            Object v = Enum.Parse(typeof (Databases), "MYSQL");   // Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.
+            Console.WriteLine((Databases)15);
+            Console.WriteLine(v);
+            Console.WriteLine(v.GetHashCode());
 
         }
     }
