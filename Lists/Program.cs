f122296f8f7f61fabb36acccad4a70afd5cfcd56
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-17 23:57:28 +0530

    Lists

---
+++ Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ListPadhlo;
+
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 5, 6 };
+        list.Add(8);
+        list.AddRange(new int[] { 1, 2, 3, 8 });  // takes any instance of a class that implements IEnumerable for eg. Array or List
+
+        foreach (int e in list)
+        {
+            Console.WriteLine(e);
+        }
+
+        Console.WriteLine("index of 8 - " + list.IndexOf(8));
+        Console.WriteLine("last index of 8 - " + list.LastIndexOf(8));
+        // Remove() doesnot work inside foreach loop
+    }
+}
