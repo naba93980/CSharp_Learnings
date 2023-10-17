305fddd5a972c9dfe1e0bd412b033508ba2610ac
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-18 00:27:54 +0530

    updated with for loop

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -8,7 +8,7 @@ internal class Program
     {
         List<int> list = new List<int>() { 5, 6 };
         list.Add(8);
-        list.AddRange(new int[] { 1, 2, 3, 8 });  // takes any instance of a class that implements IEnumerable for eg. Array or List
+        list.AddRange(new int[] { 1, 2, 3, 4 });  // takes any instance of a class that implements IEnumerable for eg. Array or List
 
         foreach (int e in list)
         {
@@ -18,5 +18,17 @@ internal class Program
         Console.WriteLine("index of 8 - " + list.IndexOf(8));
         Console.WriteLine("last index of 8 - " + list.LastIndexOf(8));
         // Remove() doesnot work inside foreach loop
+        for (var i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(i+" ----"+list[i]);
+            if(i==3){
+                list.Remove(list[i]);
+            }
+        }
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(list.IndexOf(item)+"------"+item);
+        }
     }
 }
