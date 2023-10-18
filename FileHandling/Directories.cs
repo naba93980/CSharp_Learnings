b1ba05c88bb0b544bfa1916be45451ee86118e24
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-19 01:38:17 +0530

    classes to handle directories

---
+++ FileHandling/Directories.cs
@@ -0,0 +1,20 @@
+﻿namespace FileHandling;
+
+public class Directories
+{
+    public static void RunDir(){
+        Directory.CreateDirectory(@"C:\Users\NABAJYOTI\Desktop\NewFolderBaby");
+        string[] files = Directory.GetFiles(@"C:\Users\NABAJYOTI\Desktop\pics","*.jpeg",SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+        }
+        string[] directories = Directory.GetDirectories(@"g:\Users\NABAJYOTI","*",SearchOption.TopDirectoryOnly);
+        foreach (var dir in directories)
+        {
+            Console.WriteLine(dir);
+        }
+    }
+
+    // DirectoryInfo very much similar to FileInfo
+}
