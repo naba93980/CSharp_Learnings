246b01f5c509c2abd53307f479a7bfd01602ff2e
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-19 01:38:32 +0530

    class to play with path

---
+++ FileHandling/PathClass.cs
@@ -0,0 +1,13 @@
+﻿namespace FileHandling;
+
+public class PathClass
+{
+    public static void RunPath()
+    {
+        string path = @"G:\Users\NABAJYOTI\C#_Learnings\C#_Learnings.sln";
+        Console.WriteLine("Extension:  " + Path.GetExtension(path));
+        Console.WriteLine("File Name:  " + Path.GetFileName(path));
+        Console.WriteLine("File Name without extension:  " + Path.GetFileNameWithoutExtension(path));
+        Console.WriteLine("Directory Name:  " + Path.GetDirectoryName(path));
+    }
+}
