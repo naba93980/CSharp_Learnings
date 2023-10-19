eb3aec6d9f9542f3ac598b8fd02a5c9c4334ead1
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-19 19:54:25 +0530

    constructor

---
+++ Classes/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Classes;
+
+public class Customer
+{
+    public string name;
+    readonly public int id;
+
+    public Customer(int id)
+    {
+        this.id=id;
+    }
+
+    public Customer(string name,int  id) : this(id)
+    {
+        this.name=name;
+    }
+}
