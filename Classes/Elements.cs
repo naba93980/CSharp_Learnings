7e2a2a13bde4059c61b9a1ecbae08bbf5e67c7b7
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-19 19:56:06 +0530

    initializing fields

---
+++ Classes/Elements.cs
@@ -0,0 +1,12 @@
+﻿namespace Classes;
+
+public class Elements
+{
+    public List<string> orders = new List<string>();    // initialize like this when u dont need to pass values from outside
+    readonly public List<int> rolls = new List<int>();  // readonly field can be asigned only once
+
+    Elements(List<int> rolls)
+    {
+        this.rolls = rolls;     //  // initialize like this when u  need to pass values from outside
+    }
+}
