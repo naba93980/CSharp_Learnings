7822c89d67ed7a16b6c7917e8a7fbf011bdb6001
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-20 22:26:44 +0530

    properties in c#

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -12,6 +12,15 @@ class Program
             name = "Naba",                              
             id = 1
         };
+
+
+        Property prop1 = new Property(23);
+        prop1.Name = "Nabajyoti";
+        prop1.Id = 2;
+        
+        Console.WriteLine(prop1.Name);
+        Console.WriteLine(prop1.Id);
+        Console.WriteLine(prop1.Age);
     }
 }
 
---
+++ Classes/Property.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes;
+
+public class Property
+{
+    private string? _name;
+    public string Name
+    {
+        get { return _name + " Modak"; }
+        set { _name = value; }
+    }
+    public int Id { get; set; }     // automatically creates private fields
+    public int Age { get; private set; } // setter cannot be accessed from outside the class, only getter can be accessed
+
+
+
+    public Property(int age)
+    {
+        Age = age;
+    }
+}
+
+// property - A class member that encapsulates a getter/setter for accessing a field
