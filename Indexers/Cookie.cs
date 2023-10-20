2a518f41081f8ef9598c5e7cb49eb856392e13fe
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-21 02:48:08 +0530

    indexers

---
+++ Indexers/Cookie.cs
@@ -0,0 +1,15 @@
+﻿namespace Indexers
+{
+    internal class Cookie
+    {
+        private Dictionary<string, string> _items = new Dictionary<string, string>();
+
+        // indexer
+        public string this[string item] {
+            get { return _items[item]; } 
+            set { _items[item] = value;}
+        }
+
+        // you cant have multiple indexers with same parameter type
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cookie cookie = new Cookie();
+            cookie["lunch"] = "Veg";            // without indexer it would be cookie.Item["key"] = "value"
+            cookie["Dinner"] = "Chicken";
+
+            Console.WriteLine(cookie["lunch"]);
+        }
+    }
+}
