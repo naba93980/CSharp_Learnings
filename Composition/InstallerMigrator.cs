0d4b60d1887d2280471367d08f343d13b4f075a5
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-23 16:45:26 +0530

    compostion for class association

---
+++ Composition/InstallerMigrator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Composition
+{
+    internal class Installer
+    {
+        private Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            this._logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("Installing");
+        }
+
+    }
+
+    internal class Migrator
+    {
+        private Logger _logger;
+        public Migrator(Logger logger)
+        {
+            this._logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We r migrating");
+        }
+    }
+}
---
+++ Composition/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            Migrator migrator = new Migrator(logger);
+            migrator.Migrate();
+            Installer installer = new Installer(logger);
+            installer.Install();
+
+        }
+    }
+}
